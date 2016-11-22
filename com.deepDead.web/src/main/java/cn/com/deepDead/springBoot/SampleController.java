50c07c24108422d4b3f163c92fd601df117033c1
xpxwo100 <1066923210@qq.com>
2016-11-22 20:08:32 +0800

    一些更新

---
+++ com.deepDead.web/src/main/java/cn/com/deepDead/springBoot/SampleController.java
@@ -0,0 +1,22 @@
+package cn.com.deepDead.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
